6aafd77fd72f45a97df0070a6945b069e333de46
mytharcher <mytharcher@gmail.com>
2013-03-02 00:57:20 +0800

    test on heroku

--- controllers/list.js
+++ controllers/list.js
@@ -1,12 +1,8 @@
 var approot = process.env.PWD;
-var googleAppId = process.env.GOOGLE_OAUTH.split(':');
 
 var config = require(approot + '/config');
-// var Response = require(approot + '/lib/Response');
 var util = require(approot + '/lib/util');
 var https = require('https');
-
-var GDClient = require('gdata').GDClient;
 
 var PICASA_ALBUMS_URL = 'https://picasaweb.google.com/data/feed/api/user/default';
 
@@ -21,28 +17,12 @@ exports.get = function (req, res, next) {
 	
 	var token = req.session.access_token;
 	
-	// if (token) {
-	// 	var client = new GDClient(googleAppId[0], googleAppId[1]);
-	// 	client.setAccessToken(token);
-	// 	client.get(PICASA_ALBUMS_URL, function(err, feed) {
-	// 		if (err) {
-	// 			res.send(500, err);
-	// 		} else {
-	// 			res.send(feed);
-	// 		}
-	// 	});
-	// } else {
-	// 	res.redirect('/');
-	// }
-	
-	// return;
-	
 	if (token) {
 		var result = [];
 		var proReq = https.request({
 			method: 'GET',
 			host: 'picasaweb.google.com',
-			path: '/data/feed/api/user/default',
+			path: '/data/feed/api/user/mytharcher?kind=album&alt=json',
 			headers: {
 				Authorization: 'OAuth ' + token
 			}
