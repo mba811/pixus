8360f2aa7af5e62327e3631d1e90a2a430378ab0
mytharcher <mytharcher@gmail.com>
2013-03-02 00:23:21 +0800

    test on heroku

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var approot = process.env.PWD;
 var googleAppId = process.env.GOOGLE_OAUTH.split(':');
 
 var config = require(approot + '/config');
-var Response = require(approot + '/lib/Response');
+// var Response = require(approot + '/lib/Response');
 var util = require(approot + '/lib/util');
 
 exports.get = function (req, res, next) {
